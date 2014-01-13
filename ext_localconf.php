b60049ca871e0484e5ca5ce84a56882c59df4637
Iris Schilke <ischilke@gmail.com>
2014-01-13 15:22:39 +0100

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,15 +7,17 @@ Tx_Extbase_Utility_Extension::configurePlugin(
 	$_EXTKEY,
 	'Pi1',
 	array(
-		'Bestand' => 'list, show, new, create, update, edit, delete',
-    	'Document' => 'list, show, add, create, edit, update, delete',
-    	'Person' => 'add, show, edit, list, create, update, delete',	
+		'Bestand' => 'list, show',
+		'Document' => 'list, show',
+		'Person' => 'list, show',
+		
 	),
 	// non-cacheable actions
 	array(
-		'Bestand' => 'new, create, edit, update, delete',
-    'Document' => 'add, create, edit, update, delete',
-    'Person' => 'add, create, edit, update, delete',
+		'Bestand' => '',
+		'Document' => '',
+		'Person' => '',
+		
 	)
 );
 
