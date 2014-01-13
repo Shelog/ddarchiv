c098c7df48ece275e9b3d2fda587a96a5a0bf5ae
Iris Schilke <ischilke@gmail.com>
2014-01-13 15:20:22 +0100

    Update ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -7,15 +7,14 @@ CREATE TABLE tx_ddarchiv_domain_model_bestand (
 
 
 	name varchar(255) DEFAULT '' NOT NULL,
-	description text(300,10) NOT NULL,
+	description text NOT NULL,
 	provenience text NOT NULL,
 	year_from int(4) DEFAULT '0' NOT NULL,
 	year_to int(4) DEFAULT '0' NOT NULL,
 	loc varchar(255) DEFAULT '' NOT NULL,
-	longitude double(11,6) DEFAULT '0.000000' NOT NULL,
-	latitude double(11,6) DEFAULT '0.000000' NOT NULL,
+	longitude double(11,2) DEFAULT '0.00' NOT NULL,
+	latitude double(11,2) DEFAULT '0.00' NOT NULL,
 	image text NOT NULL,
-  
 	documents varchar(255) DEFAULT '' NOT NULL,
 	persons varchar(255) DEFAULT '' NOT NULL,
 
@@ -52,21 +51,21 @@ CREATE TABLE tx_ddarchiv_domain_model_bestand (
 # Table structure for table 'tx_ddarchiv_domain_model_document'
 #
 CREATE TABLE tx_ddarchiv_domain_model_document (
-	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
+	uid int(11) NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 
 	bestand_uid int(11) DEFAULT '0' NOT NULL,
-  bestand_table tinytext NOT NULL,
+	bestand_table tinytext NOT NULL,
 
 	title varchar(255) DEFAULT '' NOT NULL,
 	description varchar(255) DEFAULT '' NOT NULL,
 	loc text NOT NULL,
 	doc_date int(11) DEFAULT '0' NOT NULL,
-	longitude double(11,6) DEFAULT '0.000000' NOT NULL,
-	latitude double(11,6) DEFAULT '0.000000' NOT NULL,
+	longitude double(11,2) DEFAULT '0.00' NOT NULL,
+	latitude double(11,2) DEFAULT '0.00' NOT NULL,
 	image text NOT NULL,
-  
-	persons int(11) unsigned DEFAULT '0' NOT NULL,
+	
+	persons varchar(255) DEFAULT '' NOT NULL,
 
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
 	crdate int(11) unsigned DEFAULT '0' NOT NULL,
@@ -103,7 +102,9 @@ CREATE TABLE tx_ddarchiv_domain_model_document (
 CREATE TABLE tx_ddarchiv_domain_model_person (
 	uid int(11) NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
-
+	
+	bestands varchar(255) DEFAULT '' NOT NULL,
+	bestands varchar(255) DEFAULT '' NOT NULL,
 
 	title varchar(255) DEFAULT '' NOT NULL,
 	name varchar(255) DEFAULT '' NOT NULL,
@@ -165,3 +166,5 @@ CREATE TABLE tx_ddarchiv_document_person_mm (
 	KEY uid_local (uid_local),
 	KEY uid_foreign (uid_foreign)
 );
+	
+
